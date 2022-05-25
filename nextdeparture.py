d499cc848f5d5d4e537ea24ad34e73a3f941a2c4
bustacheeze <bustacheeze@gmail.com>
2022-05-24 23:49:47 -0500

    misc cleanup

--- nextdeparture.py
+++ nextdeparture.py
@@ -1,5 +1,5 @@
 import sys
-import schedule
+from schedule import Schedule
 
 
 if __name__ == "__main__":
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     if (len(sys.argv) < 4):
         print(f"ERROR: Invalid arguments, expected 3 and got {len(sys.argv)-1}.\nCorrect syntax: \'python NextTrip.py <ROUTE> <STOP> <DIRECTION>\'")
         exit()
+    
+    s = Schedule(sys.argv[1], sys.argv[3], sys.argv[2])
 
-        s = Schedule(sys.argv[1], sys.argv[3], sys.argv[2])
-
-        print(s.get_next_departure())
+    print(s.get_next_departure())
